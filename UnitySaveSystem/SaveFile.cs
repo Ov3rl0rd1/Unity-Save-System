314543e6faff8c0ddc96149b989a86fa3e03dca4
Nikita Budanov <nrbudanov123@gmail.com>
2021-09-05 10:36:43 +0300

    Added simple json save system

---
+++ UnitySaveSystem/SaveFile.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace UnitySaveSystem
+{
+    public class SaveFile
+    {
+        public readonly string JsonFilePath;
+
+        public T LoadClass<T>()
+        {
+            return JsonUtility.FromJson<T>(File.ReadAllText(JsonFilePath));
+        }
+
+        public void SaveClass<T>(T classToSave)
+        {
+            try
+            {
+                JsonUtility.FromJsonOverwrite(JsonFilePath, classToSave);
+            }
+            catch
+            {
+                File.AppendAllText(JsonFilePath, JsonUtility.ToJson(classToSave));
+            }
+        }
+
+        public SaveFile(string fullSaveName)
+        {
+            JsonFilePath = fullSaveName + ".json";
+            File.Create(fullSaveName + ".json").Close();
+        }
+    }
+}
---
+++ UnitySaveSystem/UnitySaveSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace UnitySaveSystem
+{
+    public class UnitySaveSystem : MonoBehaviour
+    {
+        //public SaveFile[] GetSavesFileFromFolder(string path)
+        //{
+        //    Directory.GetFiles(path, "*.json");
+        //}
+    }
+}
